920fe70fa6654593ea529076e8594109117023e3
t4ku31 <mumimumi314@gmail.com>
2025-12-03 23:00:24 +0900

    Fix tests: Configure H2 and mock JwtDecoder for CI

--- servlet/resource/src/test/java/com/example/app1/App1ApplicationTests.java
+++ servlet/resource/src/test/java/com/example/app1/App1ApplicationTests.java
@@ -6,6 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class App1ApplicationTests {
 
+	@org.springframework.boot.test.mock.mockito.MockBean
+	org.springframework.security.oauth2.jwt.JwtDecoder jwtDecoder;
+
 	@Test
 	void contextLoads() {
 	}
