196a4774556c0114702650a2c600a27c9ed70152
t4ku31 <mumimumi314@gmail.com>
2025-12-05 11:41:46 +0900

    Fix BFF test context loading by excluding OAuth2 config classes

--- servlet/bff/src/test/java/io/reflectoring/bff/BffApplicationTests.java
+++ servlet/bff/src/test/java/io/reflectoring/bff/BffApplicationTests.java
@@ -2,8 +2,15 @@ package io.reflectoring.bff;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+import io.reflectoring.bff.config.CorsConfig;
+import io.reflectoring.bff.config.OAuth2LoginSecurityConfig;
 
 @SpringBootTest
+@ComponentScan(basePackages = "io.reflectoring.bff", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {
+		OAuth2LoginSecurityConfig.class, CorsConfig.class }))
 class BffApplicationTests {
 
 	@Test
